36de4f6ec1b754a7d60f4d74349b82979ec1034d
Danil Nikolaev <dan369danil@yandex.ru>
2025-03-16 20:21:57 +0300

    Update files

--- 01/predict_package/predict.py
+++ 01/predict_package/predict.py
@@ -1,15 +1,12 @@
-import hashlib
 
 
 class SomeModel:
     def predict(self, message: str) -> float:
-        # Реализация не важна.  В данном случае сделаем заглушку, возвращающую разные значения для разных сообщений
         if message == "Чапаев и пустота":
-            return 0.9  # Чтобы проходил первый assert
-        elif message == "Вулкан":
-            return 0.2  # Чтобы проходил третий assert
-        else:
-            return 0.5  # Значение по умолчанию, чтобы проходил assert, если ни один из if не сработал
+            return 0.9
+        if message == "Вулкан":
+            return 0.2
+        return 0.5
 
 
 def predict_message_mood(
@@ -24,10 +21,3 @@ def predict_message_mood(
     if value > good_thresholds:
         return 'отл'
     return 'норм'
-
-
-
-# assert predict_message_mood("Чапаев и пустота") == "отл"
-# assert predict_message_mood("Чапаев и пустота", 0.8, 0.99) == "норм"
-# assert predict_message_mood("Вулкан") == "неуд"
-
--- 01/tests/test_predict.py
+++ 01/tests/test_predict.py
@@ -1,5 +1,6 @@
-from predict_package.predict import predict_message_mood
-# Тесты на основе заглушки:
+from ..predict_package.predict import predict_message_mood
+
+
 def test_predict_positive_message():
     assert predict_message_mood("Чапаев и пустота") == "отл"
 
@@ -11,17 +12,18 @@ def test_predict_negative_message():
 def test_predict_default_message():
     assert predict_message_mood("Some other message") == "норм"
 
-# Граничные значения
+
 def test_predict_at_bad_threshold():
-    assert predict_message_mood("Some other message", bad_thresholds=0.5) == "норм" #значение с заглушки равно 0.5
+    assert predict_message_mood("Some other message", bad_thresholds=0.5) == "норм"
 
 
 def test_predict_at_good_threshold():
-     assert predict_message_mood("Some other message", good_thresholds=0.5) == "норм"
+    assert predict_message_mood("Some other message", good_thresholds=0.5) == "норм"
 
-# Комбинированные тесты с разными порогами
+
 def test_predict_custom_thresholds_1():
     assert predict_message_mood("Some other message", bad_thresholds=0.6, good_thresholds=0.7) == "неуд"
+
 
 def test_predict_custom_thresholds_2():
     assert predict_message_mood("Some other message", bad_thresholds=0.1, good_thresholds=0.4) == "отл"
@@ -30,42 +32,14 @@ def test_predict_custom_thresholds_2():
 def test_predict_custom_thresholds_3():
     assert predict_message_mood("Some other message", bad_thresholds=0.1, good_thresholds=0.9) == "норм"
 
-# Передача параметров из модели:
+
 def test_predict_model_return_value():
-    # Для этого теста придется временно изменить заглушку и посмотреть какое значение выдает Positive!
-    # Можно изменять SomeModel чтобы проверить логику predict_message_mood
-    # Тестируем, что predict_message_mood возвращает "отл", когда модель выдает значение больше порогового значения.
-    # Временно меняем модель
-    assert predict_message_mood("Чапаев и пустота", bad_thresholds = 0.1, good_thresholds = 0.85) == "отл" # 0.9
+    assert predict_message_mood("Чапаев и пустота", bad_thresholds=0.1, good_thresholds=0.85) == "отл"
 
 
-# Тесты на пустую строку: (проверка, что не ломается ничего)
 def test_predict_empty_string():
     assert predict_message_mood("") == "норм"
 
-# Тест с юникодом (чтобы работало с русскими буквами)
-def test_predict_unicode():
-    assert predict_message_mood("Какое-то позитивное сообщение") == "норм"
 
-def test_predict_at_good_threshold():
-     assert predict_message_mood("Some other message", good_thresholds=0.5) == "норм"
-
-# Комбинированные тесты с разными порогами
-def test_predict_custom_thresholds_1():
-    assert predict_message_mood("Some other message", bad_thresholds=0.6, good_thresholds=0.7) == "неуд"
-
-def test_predict_custom_thresholds_2():
-    assert predict_message_mood("Some other message", bad_thresholds=0.1, good_thresholds=0.4) == "отл"
-
-def test_predict_custom_thresholds_3():
-    assert predict_message_mood("Some other message", bad_thresholds=0.1, good_thresholds=0.9) == "норм"
-
-
-
-# Тесты на пустую строку: (проверка, что не ломается ничего)
-def test_predict_empty_string():
-    assert predict_message_mood("") == "норм"
-
-# Тест с юникодом (чтобы работало с русскими буквами)
 def test_predict_unicode():
     assert predict_message_mood("Какое-то позитивное сообщение") == "норм"
