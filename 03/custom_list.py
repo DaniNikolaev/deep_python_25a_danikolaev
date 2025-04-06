83e2a29f459b3711f5e0b600c3222353456cf3c8
Danil Nikolaev <dan369danil@yandex.ru>
2025-04-06 17:42:19 +0300

    Обновление 03 и pylintrc

--- 03/custom_list.py
+++ 03/custom_list.py
@@ -2,69 +2,62 @@
 
 class CustomList(list):
 
-    def __add__(self, other):
+    def __calculate(self, other, op='+', r_op=False):
         if isinstance(other, int):
-            values = [v + other for v in self]
+            if op == '+':
+                values = [v + other for v in self]
+            else:
+                if r_op:
+                    values = [other - v for v in self]
+                else:
+                    values = [v - other for v in self]
             return CustomList(values)
         if type(other) in (list, CustomList):
             diff = len(self) - len(other)
             if diff == 0:
-                values = [v + o for v, o in zip(self, other)]
+                if op == '+':
+                    values = [v + o for v, o in zip(self, other)]
+                else:
+                    if r_op:
+                        values = [o - v for v, o in zip(self, other)]
+                    else:
+                        values = [v - o for v, o in zip(self, other)]
                 return CustomList(values)
             if diff < 0:
-                self.extend([0] * (len(other) - len(self)))
-                values = [v + o for v, o in zip(self, other)]
+                self_copy = self.copy()
+                self_copy.extend([0] * (-diff))
+                if op == '+':
+                    values = [v + o for v, o in zip(self_copy, other)]
+                else:
+                    if r_op:
+                        values = [o - v for v, o in zip(self_copy, other)]
+                    else:
+                        values = [v - o for v, o in zip(self_copy, other)]
                 return CustomList(values)
             if diff > 0:
-                other.extend([0] * (len(self) - len(other)))
-                values = [v + o for v, o in zip(self, other)]
+                other_copy = other.copy()
+                other_copy.extend([0] * diff)
+                if op == '+':
+                    values = [v + o for v, o in zip(self, other_copy)]
+                else:
+                    if r_op:
+                        values = [o - v for v, o in zip(self, other_copy)]
+                    else:
+                        values = [v - o for v, o in zip(self, other_copy)]
                 return CustomList(values)
         return None
+
+    def __add__(self, other):
+        return self.__calculate(other)
 
     def __radd__(self, other):
-        return self + other
+        return self.__calculate(other, r_op=True)
 
     def __sub__(self, other):
-        if isinstance(other, int):
-            values = [v - other for v in self]
-            return CustomList(values)
-        if type(other) in (list, CustomList):
-            diff = len(self) - len(other)
-            if diff == 0:
-                values = [v - o for v, o in zip(self, other)]
-                return CustomList(values)
-            if diff < 0:
-                temp_values = self.copy()
-                temp_values.extend([0] * (len(other) - len(self)))
-                values = [v - o for v, o in zip(temp_values, other)]
-                return CustomList(values)
-            if diff > 0:
-                temp_values = other.copy()
-                temp_values.extend([0] * (len(self) - len(other)))
-                values = [v - o for v, o in zip(self, temp_values)]
-                return CustomList(values)
-        return None
+        return self.__calculate(other, op='-')
 
     def __rsub__(self, other):
-        if isinstance(other, int):
-            values = [other - v for v in self]
-            return CustomList(values)
-        if type(other) in (list, CustomList):
-            diff = len(self) - len(other)
-            if diff == 0:
-                values = [o - v for v, o in zip(self, other)]
-                return CustomList(values)
-            if diff < 0:
-                temp_values = self.copy()
-                temp_values.extend([0] * (len(other) - len(self)))
-                values = [o - v for v, o in zip(temp_values, other)]
-                return CustomList(values)
-            if diff > 0:
-                temp_values = other.copy()
-                temp_values.extend([0] * (len(self) - len(other)))
-                values = [o - v for v, o in zip(self, temp_values)]
-                return CustomList(values)
-        return None
+        return self.__calculate(other, op='-', r_op=True)
 
     def __eq__(self, other):
         return sum(self) == sum(other)
