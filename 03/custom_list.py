ea2c78341fc826cc45b45ac24d429a4122e74db0
Danil Nikolaev <dan369danil@yandex.ru>
2025-04-12 11:52:28 +0300

    обновление 03 v2

--- 03/custom_list.py
+++ 03/custom_list.py
@@ -1,66 +1,49 @@
+from itertools import zip_longest
 
 
 class CustomList(list):
 
-    def __calculate(self, other, op='+', r_op=False):
+    @staticmethod
+    def __calculate_values(val1, val2, op, r_op):
+        if op == '+':
+            return val1 + val2
+        return val2 - val1 if r_op else val1 - val2
+
+    def __process_operation(self, other, op='+', r_op=False):
         if isinstance(other, int):
-            if op == '+':
-                values = [v + other for v in self]
-            else:
-                if r_op:
-                    values = [other - v for v in self]
-                else:
-                    values = [v - other for v in self]
-            return CustomList(values)
-        if type(other) in (list, CustomList):
-            diff = len(self) - len(other)
-            if diff == 0:
-                if op == '+':
-                    values = [v + o for v, o in zip(self, other)]
-                else:
-                    if r_op:
-                        values = [o - v for v, o in zip(self, other)]
-                    else:
-                        values = [v - o for v, o in zip(self, other)]
-                return CustomList(values)
-            if diff < 0:
-                self_copy = self.copy()
-                self_copy.extend([0] * (-diff))
-                if op == '+':
-                    values = [v + o for v, o in zip(self_copy, other)]
-                else:
-                    if r_op:
-                        values = [o - v for v, o in zip(self_copy, other)]
-                    else:
-                        values = [v - o for v, o in zip(self_copy, other)]
-                return CustomList(values)
-            if diff > 0:
-                other_copy = other.copy()
-                other_copy.extend([0] * diff)
-                if op == '+':
-                    values = [v + o for v, o in zip(self, other_copy)]
-                else:
-                    if r_op:
-                        values = [o - v for v, o in zip(self, other_copy)]
-                    else:
-                        values = [v - o for v, o in zip(self, other_copy)]
-                return CustomList(values)
+            return CustomList([self.__calculate_values(v, other, op, r_op) for v in self])
+
+        if isinstance(other, (list, CustomList)):
+            return CustomList([
+                self.__calculate_values(v1, v2, op, r_op)
+                for v1, v2 in zip_longest(self, other, fillvalue=0)
+            ])
+
         return None
 
     def __add__(self, other):
-        return self.__calculate(other)
+        return self.__process_operation(other)
 
     def __radd__(self, other):
-        return self.__calculate(other, r_op=True)
+        return self.__process_operation(other, r_op=True)
 
     def __sub__(self, other):
-        return self.__calculate(other, op='-')
+        return self.__process_operation(other, op='-')
 
     def __rsub__(self, other):
-        return self.__calculate(other, op='-', r_op=True)
+        return self.__process_operation(other, op='-', r_op=True)
 
     def __eq__(self, other):
-        return sum(self) == sum(other)
+        if len(self) != len(other):
+            return False
+        flag = all((v == o for v, o in zip(self, other)))
+        return sum(self) == sum(other) and flag
+
+    def __ne__(self, other):
+        if len(self) != len(other):
+            return True
+        flag = any((v != o for v, o in zip(self, other)))
+        return sum(self) != sum(other) or flag
 
     def __lt__(self, other):
         return sum(self) < sum(other)
@@ -68,8 +51,11 @@ class CustomList(list):
     def __le__(self, other):
         return sum(self) <= sum(other)
 
+    def __gt__(self, other):
+        return sum(self) > sum(other)
+
+    def __ge__(self, other):
+        return sum(self) >= sum(other)
+
     def __str__(self):
-        s = ''
-        for v in self:
-            s += str(v) + ' '
-        return s + 'Сумма: ' + str(sum(self))
+        return f"{' '.join(map(str, self))} Сумма: {sum(self)}"
--- 03/test_custom_list.py
+++ 03/test_custom_list.py
@@ -1,9 +1,23 @@
-from unittest.mock import patch
 import pytest
 from custom_list import CustomList
 
 
 class TestCustomList:
+    @staticmethod
+    def check_original_unchanged(original1, original2, operation):
+        original1_copy = original1.copy()
+        original2_copy = original2.copy()\
+            if hasattr(original2, 'copy') \
+            else list(original2) if isinstance(original2, (list, CustomList)) else original2
+
+        result = operation()
+
+        assert list(original1) == list(original1_copy)
+        if isinstance(original2, (list, CustomList)):
+            assert list(original2) == list(original2_copy)
+        else:
+            assert original2 == original2_copy
+        return result
 
     @pytest.mark.parametrize(
         "list1, list2, expected",
@@ -19,8 +33,16 @@ class TestCustomList:
     )
     def test_add(self, list1, list2, expected):
         cl1 = CustomList(list1)
-        result = cl1 + list2
-        assert result == CustomList(expected)
+        other = list2
+
+        def operation():
+            return cl1 + other
+
+        result = self.check_original_unchanged(cl1, other, operation)
+
+        assert isinstance(result, CustomList)
+        assert list(result) == expected
+        assert sum(result) == sum(expected)
 
     def test_add_invalid_type(self):
         cl = CustomList([1, 2, 3])
@@ -41,8 +63,16 @@ class TestCustomList:
     )
     def test_radd(self, list1, list2, expected):
         cl1 = CustomList(list1)
-        result = list2 + cl1
-        assert result == CustomList(expected)
+        other = list2
+
+        def operation():
+            return other + cl1
+
+        result = self.check_original_unchanged(cl1, other, operation)
+
+        assert isinstance(result, CustomList)
+        assert list(result) == expected
+        assert sum(result) == sum(expected)
 
     @pytest.mark.parametrize(
         "list1, list2, expected",
@@ -58,8 +88,16 @@ class TestCustomList:
     )
     def test_sub(self, list1, list2, expected):
         cl1 = CustomList(list1)
-        result = cl1 - list2
-        assert result == CustomList(expected)
+        other = list2
+
+        def operation():
+            return cl1 - other
+
+        result = self.check_original_unchanged(cl1, other, operation)
+
+        assert isinstance(result, CustomList)
+        assert list(result) == expected
+        assert sum(result) == sum(expected)
 
     @pytest.mark.parametrize(
         "list1, list2, expected",
@@ -75,20 +113,64 @@ class TestCustomList:
     )
     def test_rsub(self, list1, list2, expected):
         cl1 = CustomList(list1)
-        result = list2 - cl1
-        assert result == CustomList(expected)
+        other = list2
+
+        def operation():
+            return other - cl1
+
+        result = self.check_original_unchanged(cl1, other, operation)
+
+        assert isinstance(result, CustomList)
+        assert list(result) == expected
+        assert sum(result) == sum(expected)
 
     @pytest.mark.parametrize(
         "list1, list2, expected",
         [
-            ([1, 2, 3], [3, 2, 1], True),
+            ([1, 2, 3], [1, 2, 3], True),
+            ([1, 2, 3], [3, 2, 1], False),
             ([1, 2, 3], [1, 2, 4], False),
+            ([1, 2, 3], [6], False),
+            ([1, 2, 3], [1, 2], False),
+            ([], [], True),
+            ([1, 2, 3], CustomList([1, 2, 3]), True),
+            ([1, 2, 3], [1, 2, 3], True),
         ],
     )
     def test_eq(self, list1, list2, expected):
         cl1 = CustomList(list1)
-        cl2 = CustomList(list2)
+        if isinstance(list2, CustomList):
+            cl2 = list2
+        else:
+            cl2 = CustomList(list2) if isinstance(list2, list) else list2
+
         assert (cl1 == cl2) == expected
+        if isinstance(cl2, (CustomList, list)):
+            assert (cl2 == cl1) == expected
+
+    @pytest.mark.parametrize(
+        "list1, list2, expected",
+        [
+            ([1, 2, 3], [1, 2, 3], False),
+            ([1, 2, 3], [3, 2, 1], True),
+            ([1, 2, 3], [1, 2, 4], True),
+            ([1, 2, 3], [6], True),
+            ([1, 2, 3], [1, 2], True),
+            ([], [], False),
+            ([1, 2, 3], CustomList([1, 2, 4]), True),
+            ([1, 2, 3], [1, 2, 4], True),
+        ],
+    )
+    def test_ne(self, list1, list2, expected):
+        cl1 = CustomList(list1)
+        if isinstance(list2, CustomList):
+            cl2 = list2
+        else:
+            cl2 = CustomList(list2) if isinstance(list2, list) else list2
+
+        assert (cl1 != cl2) == expected
+        if isinstance(cl2, (CustomList, list)):
+            assert (cl2 != cl1) == expected
 
     @pytest.mark.parametrize(
         "list1, list2, expected",
@@ -96,6 +178,8 @@ class TestCustomList:
             ([1, 2, 3], [1, 2, 4], True),
             ([1, 2, 3], [1, 2, 2], False),
             ([1, 2, 3], [1, 2, 3], False),
+            ([1, 2, 3], [6], False),
+            ([], [1], True),
         ],
     )
     def test_lt(self, list1, list2, expected):
@@ -109,6 +193,9 @@ class TestCustomList:
             ([1, 2, 3], [1, 2, 4], True),
             ([1, 2, 3], [1, 2, 3], True),
             ([1, 2, 3], [1, 2, 2], False),
+            ([1, 2, 3], [6], True),
+            ([], [1], True),
+            ([], [], True),
         ],
     )
     def test_le(self, list1, list2, expected):
@@ -116,54 +203,49 @@ class TestCustomList:
         cl2 = CustomList(list2)
         assert (cl1 <= cl2) == expected
 
+    @pytest.mark.parametrize(
+        "list1, list2, expected",
+        [
+            ([1, 2, 3], [1, 2, 2], True),
+            ([1, 2, 3], [1, 2, 3], False),
+            ([1, 2, 3], [1, 2, 4], False),
+            ([6], [1, 2, 3], False),
+            ([1], [], True),
+        ],
+    )
+    def test_gt(self, list1, list2, expected):
+        cl1 = CustomList(list1)
+        cl2 = CustomList(list2)
+        assert (cl1 > cl2) == expected
+
+    @pytest.mark.parametrize(
+        "list1, list2, expected",
+        [
+            ([1, 2, 3], [1, 2, 2], True),
+            ([1, 2, 3], [1, 2, 3], True),
+            ([1, 2, 3], [1, 2, 4], False),
+            ([6], [1, 2, 3], True),
+            ([1], [], True),
+            ([], [], True),
+        ],
+    )
+    def test_ge(self, list1, list2, expected):
+        cl1 = CustomList(list1)
+        cl2 = CustomList(list2)
+        assert (cl1 >= cl2) == expected
+
     def test_str(self):
         cl = CustomList([1, 2, 3])
         assert str(cl) == "1 2 3 Сумма: 6"
+        assert str(CustomList([])) == " Сумма: 0"
 
-    def test_add_extend_side_effect(self):
-        cl = CustomList([1, 2])
-        other = [4, 5, 6]
-        cl_copy = cl.copy()
-        other_copy = other.copy()
+    def test_comparison_with_regular_list(self):
+        cl = CustomList([1, 2, 3])
+        regular_list = [1, 2, 3]
 
-        with patch.object(CustomList, '_CustomList__calculate') as mock_calculate:
-            _ = cl + other
-            if len(cl_copy) < len(other_copy):
-                assert mock_calculate.call_count > 0
-            else:
-                assert mock_calculate.call_count > 0
-
-            assert cl == cl_copy
-            assert other == other_copy
-
-    def test_sub_extend_side_effect(self):
-        cl = CustomList([1, 2])
-        other = [4, 5, 6]
-        cl_copy = cl.copy()
-        other_copy = other.copy()
-
-        with patch.object(CustomList, '_CustomList__calculate') as mock_calculate:
-            _ = cl - other
-            if len(cl_copy) < len(other_copy):
-                assert mock_calculate.call_count > 0
-            else:
-                assert mock_calculate.call_count > 0
-
-            assert cl == cl_copy
-            assert other == other_copy
-
-    def test_rsub_extend_side_effect(self):
-        cl = CustomList([1, 2])
-        other = [4, 5, 6]
-        cl_copy = cl.copy()
-        other_copy = other.copy()
-
-        with patch.object(CustomList, '_CustomList__calculate') as mock_calculate:
-            _ = other - cl
-            if len(cl_copy) > len(other_copy):
-                assert mock_calculate.call_count > 0
-            else:
-                assert mock_calculate.call_count > 0
-
-            assert cl == cl_copy
-            assert other == other_copy
+        assert (cl == regular_list) is True
+        assert (cl != regular_list) is False
+        assert (cl < [1, 2, 4]) is True
+        assert (cl <= [1, 2, 3]) is True
+        assert (cl > [1, 2, 2]) is True
+        assert (cl >= [1, 2, 3]) is True
