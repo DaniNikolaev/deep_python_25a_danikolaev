aaf30b3ccf3788a4281fb678bc0184081a1c2097
Danil Nikolaev <dan369danil@yandex.ru>
2025-04-16 17:16:28 +0300

    исправление 05 v.1

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -87,18 +87,3 @@ class LRUCache:
             s += str(node) + '\n'
             node = node.next_node
         return s
-
-cache = LRUCache(2)
-
-cache.set("k1", "val1")
-cache.set("k2", "val2")
-
-assert cache.get("k3") is None
-assert cache.get("k2") == "val2"
-assert cache.get("k1") == "val1"
-
-cache.set("k3", "val3")
-
-assert cache.get("k3") == "val3"
-assert cache.get("k2") is None
-assert cache.get("k1") == "val1"
